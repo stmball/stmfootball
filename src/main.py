d63669331eb414fc1613454fc29956a5042660f2
Sam Ball <stmball@gmail.com>
2023-07-28 13:45:56 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""Placeholder code to test things are working properly."""
+
+
+def main() -> bool:
+    """Run a simple function that returns True."""
+    return True
+
+
+if __name__ == "__main__":
+    main()
